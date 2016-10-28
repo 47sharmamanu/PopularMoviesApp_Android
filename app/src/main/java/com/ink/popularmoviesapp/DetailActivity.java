6d946548814db03358207a65cfc8d4b302b52a5d
47sharmamanu <47sharma.manu@gmail.com>
2016-10-28 22:56:30 +0530

    Saving instance.

--- app/src/main/java/com/ink/popularmoviesapp/DetailActivity.java
+++ app/src/main/java/com/ink/popularmoviesapp/DetailActivity.java
@@ -1,7 +1,7 @@
 package com.ink.popularmoviesapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,16 +10,18 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail2);
 
-        //setting values(Uri) in argument of fragment
-        Bundle arguments = new Bundle();
-        arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
-        DetailFragment df = new DetailFragment();
-        if (null != df) {
-            df.setArguments(arguments);
-        }
+        if (savedInstanceState == null) {
+            //setting values(Uri) in argument of fragment
+            Bundle arguments = new Bundle();
+            arguments.putParcelable(DetailFragment.DETAIL_URI, getIntent().getData());
+            DetailFragment df = new DetailFragment();
+            if (null != df) {
+                df.setArguments(arguments);
+            }
 
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.activity_detail2, df)
-                .commit();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.activity_detail2, df)
+                    .commit();
+        }
     }
 }
